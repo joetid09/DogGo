f7697994aad67f3f29430699094e1b5ae5ef42f4
joseph tidwell <joseph.tidwell09@gmail.com>
2020-12-13 11:00:02 -0600

    updated delete functions, can not navigate to and delete a dog

--- DogGo/Repositories/DogRepository.cs
+++ DogGo/Repositories/DogRepository.cs
@@ -138,6 +138,27 @@ namespace DogGo.Repositories
                 }
             }
         }
+
+        public void DeleteDog(int dogId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Dog
+                            WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", dogId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
 
--- DogGo/Repositories/IDogRepository.cs
+++ DogGo/Repositories/IDogRepository.cs
@@ -9,5 +9,6 @@ namespace DogGo.Repositories
         Dog GetDogById(int id);
         void AddDog(Dog dog);
         void UpdateDog(Dog dog);
+        void DeleteDog(int dogId);
     }
 }
