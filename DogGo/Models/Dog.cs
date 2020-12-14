ab27004bd1cb0814594a9899514b4bf3bd231a08
joseph tidwell <joseph.tidwell09@gmail.com>
2020-12-14 12:02:39 -0600

    updated DogRepository with GetDogByOwnerId, Walker repository by GetByNeighborhood, and added the methods to their corresponding interfaces

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -8,6 +8,8 @@ namespace DogGo.Models
         public string Name { get; set; }
         public int OwnerId { get; set; }
         public string Breed { get; set; }
+        public string Notes { get; set }
+        public string ImageUrl { get; set; }
 
     }
 }
--- DogGo/Repositories/IDogRepository.cs
+++ DogGo/Repositories/IDogRepository.cs
@@ -10,5 +10,6 @@ namespace DogGo.Repositories
         void AddDog(Dog dog);
         void UpdateDog(Dog dog);
         void DeleteDog(int dogId);
+        void GetDogsByOwnerId(int ownerId);
     }
 }
