3ab6982d2145bfef6c671e8c86d1e0f24af6ee9b
joseph tidwell <joseph.tidwell09@gmail.com>
2020-12-17 12:06:25 -0600

    removed edit and delete options from doggo list, made walkers avatar a button to details

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,13 +1,18 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int OwnerId { get; set; }
+        [Required]
         public string Breed { get; set; }
+        [StringLength(250, MinimumLength = 0)]
         public string Notes { get; set; }
         public string ImageUrl { get; set; }
 
--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,9 +1,14 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Let the world know your name!")]
+        [MaxLength(35)]
         public string Name { get; set; }
+        [Required(ErrorMessage ="What part of town do you work in?")]
         public int NeighborhoodId { get; set; }
         public string ImageUrl { get; set; }
         public Neighborhood Neighborhood { get; set; }
