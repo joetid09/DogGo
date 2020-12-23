b6f0c79eacda6388ae8b44afb216fae34a57626f
joseph tidwell <joseph.tidwell09@gmail.com>
2020-12-22 18:02:34 -0600

    fixed issue regarding catch not firing, now loads avm if caught

--- DogGo/Models/ViewModels/AppointmentViewModel.cs
+++ DogGo/Models/ViewModels/AppointmentViewModel.cs
@@ -7,10 +7,12 @@ namespace DogGo.Models.ViewModels
 {
     public class AppointmentViewModel
     {
+
+        
         public int OwnerId { get; set; }
         public Walker Walker { get; set; }
         public List<Dog> Doggos { get; set; }
         public Walks Walk { get; set; }
-
+        public string ErrorMessage { get; set; }
     }
 }
