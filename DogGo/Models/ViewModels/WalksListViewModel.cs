5eb788281112b0ffb009cbad9d82d940b76f80ad
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-03 21:03:49 -0600

    created new walks view modelto tie into walker and owner profiles

---
+++ DogGo/Models/ViewModels/WalksListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        List<Dog> Dogs { get; set; }
+        List<Walks> Walks { get; set; }
+        List<Owner> Owners { get; set; }
+    }
+}
